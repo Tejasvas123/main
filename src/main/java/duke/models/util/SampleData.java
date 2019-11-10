341f3e6c47b317876198d783e88b7a3f8d14c2f7
tejas123 <vasavada.tejas@gmail.com>
2019-11-11 01:42:38 +0800

    checkstyle

--- src/main/java/duke/models/util/SampleData.java
+++ src/main/java/duke/models/util/SampleData.java
@@ -13,34 +13,37 @@ import duke.models.tag.Tag;
  */
 public class SampleData {
 
-    public static Locker[] getSampleLockers() throws DukeException {
+    private static Locker[] getSampleLockers() throws DukeException {
         return new Locker[]{
-                new Locker(new SerialNumber("1"), new Address("Com-1"), new Zone("A"),
-                        new Tag(Tag.NOT_IN_USE), null),
-                new Locker(new SerialNumber(("2")), new Address("Com-1"), new Zone("A"),
-                        new Tag(Tag.NOT_IN_USE), null),
-                new Locker(new SerialNumber(("3")), new Address("Com-1"), new Zone("A"),
-                        new Tag(Tag.NOT_IN_USE), null),
-                new Locker(new SerialNumber("4"), new Address("Com-1"), new Zone("A"),
-                        new Tag(Tag.UNAUTHORIZED), null),
-                new Locker(new SerialNumber("5"), new Address("Com-1"), new Zone("A"),
-                        new Tag(Tag.BROKEN), null),
-                new Locker(new SerialNumber("6"), new Address("Com-2"), new Zone("B"),
-                        new Tag(Tag.NOT_IN_USE), null),
-                new Locker(new SerialNumber("7"), new Address("Com-2"), new Zone("B"),
-                        new Tag(Tag.UNAUTHORIZED), null),
-                new Locker(new SerialNumber("8"), new Address("Com-2"), new Zone("C"),
-                        new Tag(Tag.BROKEN), null),
-                new Locker(new SerialNumber("9"), new Address("Com-2"), new Zone("C"),
-                        new Tag(Tag.NOT_IN_USE), null),
-                new Locker(new SerialNumber("10"), new Address("Com-2"), new Zone("C"),
-                        new Tag(Tag.NOT_IN_USE), null)
+            new Locker(new SerialNumber("1"), new Address("Com-1"), new Zone("A"),
+                       new Tag(Tag.NOT_IN_USE), null),
+            new Locker(new SerialNumber(("2")), new Address("Com-1"), new Zone("A"),
+                       new Tag(Tag.NOT_IN_USE), null),
+            new Locker(new SerialNumber(("3")), new Address("Com-1"), new Zone("A"),
+                       new Tag(Tag.NOT_IN_USE), null),
+            new Locker(new SerialNumber("4"), new Address("Com-1"), new Zone("A"),
+                       new Tag(Tag.UNAUTHORIZED), null),
+            new Locker(new SerialNumber("5"), new Address("Com-1"), new Zone("A"),
+                       new Tag(Tag.BROKEN), null),
+            new Locker(new SerialNumber("6"), new Address("Com-2"), new Zone("B"),
+                       new Tag(Tag.NOT_IN_USE), null),
+            new Locker(new SerialNumber("7"), new Address("Com-2"), new Zone("B"),
+                       new Tag(Tag.UNAUTHORIZED), null),
+            new Locker(new SerialNumber("8"), new Address("Com-2"), new Zone("C"),
+                       new Tag(Tag.BROKEN), null),
+            new Locker(new SerialNumber("9"), new Address("Com-2"), new Zone("C"),
+                       new Tag(Tag.NOT_IN_USE), null),
+            new Locker(new SerialNumber("10"), new Address("Com-2"), new Zone("C"),
+                       new Tag(Tag.NOT_IN_USE), null)
         };
     }
 
+    /**
+     * Returns the sample list of lockers.
+     */
     public static LockerList getSampleLockerList() throws DukeException {
         LockerList sampleList = new LockerList();
-        for (Locker sampleLocker: getSampleLockers()) {
+        for (Locker sampleLocker : getSampleLockers()) {
             sampleList.addLocker(sampleLocker);
         }
         return sampleList;
